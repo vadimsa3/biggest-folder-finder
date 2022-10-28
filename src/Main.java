b988756d943f763ce23e4731ab4faf6595503dbc
vadim_savchuk <vadimsa3@gmail.com>
2022-10-28 08:59:59 +0300

    Метод подсчета размера папки

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String folderPath = "C:/Users/savchuk/Desktop/Новая папка";
+        File file = new File(folderPath);
+        System.out.println(getFolderSize(file));
+    }
+
+    // создаем метод для подсчета размера папки суммируя размеры файлов
+    public static long getFolderSize(File folder) {
+        if (folder.isFile()) {      // проверяем является-ли папка файлом
+            return folder.length(); // если да, получаем размер файла
+        }
+        long sum = 0;
+        File[] files = folder.listFiles();  // получаем список файлов в этой папке
+        for (File file : files) {           // проходим по всем файлам
+            sum += getFolderSize(file);     // суммируем размеры всех файлов всех папок
+        }
+        return sum;
     }
 }
